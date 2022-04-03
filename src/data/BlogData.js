2708de8b43c9607aa03db58def9b05be0303e537
hrb11182 <59706923+hrb11182@users.noreply.github.com>
2022-04-03 07:58:18 +0530

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,20 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Object Oriented Programming in C++",
+        tags:["C++","Object Oriented Programming"],
+        date:"2 April, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--BQrhO_G_--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/gfnx83nx2kounrj8j5ra.jpeg",
+        link:"https://dev.to/hrb11182/object-oriented-programming-in-c-45j9"
+    },
+    {
+        id:2,
+        name:"Git & Github...",
+        tags:["Git","Version-control","Github"],
+        date:"5 April, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--IOW98qEl--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/lh820vpp2t5qaa1n3igz.jpeg",
+        link:"https://dev.to/hrb11182/git-github-3mmi"
+    },
+] 
